2b2f1fae0dd83bd021bb01686f0786d2132e8a37
Markus <mhuber@anidynamics.com>
2021-04-11 23:25:16 +0200

    create dataframe and write to output folder

--- ml-data-prep/create-dataframe.py
+++ ml-data-prep/create-dataframe.py
@@ -3,26 +3,37 @@ import click
 import pandas as pd
 
 
-def create_dfs_from_folders(path_to_folders):
+def create_dfs_from_folders(path_to_folders, output_directory):
+    # create a DataFrame from folders
     print("Creating dataframes from folders in " + path_to_folders + " ...")
     df = pd.DataFrame()
-    for folder in os.scandir(path_to_folders):        
-        for image_file in os.scandir(folder):
-            df = df.append({'filename': os.path.basename(image_file), 'image_path': os.path.abspath(image_file), 'label': os.path.basename(folder)}, ignore_index=True)
-    print("done","\n")
-    df.to_csv(os.path.join(path_to_folders, 'df.csv'))
+    for folder in os.scandir(path_to_folders):
+        if os.path.isdir(folder):
+            for image_file in os.scandir(folder):
+                df = df.append({'filename': os.path.basename(image_file), 'image_path': os.path.abspath(image_file), 'label': os.path.basename(folder)}, ignore_index=True)
+    print("Done.")
+
+    # write DataFrame to csv file
+    df_path = os.path.join(output_directory, 'df.csv')
+    print("Wrinting dataframee to " + os.path.join(output_directory, 'df.csv') + " ...")
+    df.to_csv(df_path)
+    print("Done.")
     return
 
 
 @click.command()
 @click.help_option('--help', '-h')
 @click.option('--path', '-p', type=click.Path(exists=True), help='Path to folders')
-# @click.option('--out_dir', '-o', help='Output directory')
-def create_dataframe(path):
+@click.option('--out_dir', '-o', help='Output directory of dataframe. Default: path to folders')
+def create_dataframe(path, out_dir):
     if path == None:
         print("No path given")
+        return
     else:
-        create_dfs_from_folders(path)
+        if out_dir == "" or out_dir == None:
+            out_dir = path
+        create_dfs_from_folders(path, out_dir)
+
 
 if __name__ == "__main__":
     create_dataframe()
